699b5b1d5f931e35b8d1a37d796f2abbd6bc6b52
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-31 07:39:04 +0600

    user set

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Users from './components/Users';
 
 const App = () => {
   // step1 : declare three states here : users, isLoading, error
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  const [error, setError] = useState('');
+
 
   // step2 : use useEffect for fetching the data including updating isLoading and error states
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data);
+      setIsloading(false)
+    })
+    .catch(error => setError(error))
+  }, [])
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
       {isLoading && <p>Loading users...</p>}
       {error && <p>{error}</p>}
-      {/* step3 : pass the users data to Users component  */}
+      {<Users users={users}></Users>}
     </div>
   );
 };
