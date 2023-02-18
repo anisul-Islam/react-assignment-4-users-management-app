a17c6ba3ce7358434318f11061cf197975ce9be9
Abdul Gaffar <abdulgaffarefti66@gmail.com>
2023-02-18 19:52:51 +0600

    React useEffect assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+/* eslint-disable */
+import React,{useState,useEffect} from 'react';
 
 import Users from './components/Users';
 
 const App = () => {
   // step1 : declare three states here : users, isLoading, error
-
+  const[users, setUsers]=useState(null);
+  const[isLoading, setLoading]=useState(true);
+  const[error, setError]=useState(null);
+  
   // step2 : use useEffect for fetching the data including updating isLoading and error states
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      if (!res.ok) {
+        throw new Error('Data is not loading!')
+      } else {
+        return res.json();
+      }
+    })
+    .then((res)=>{
+      setUsers(res)
+      setLoading(false)
+    }).catch((error)=>{
+      setError(error.message)
+      setLoading(false)
+    })
+  },[])
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
-      {isLoading && <p>Loading users...</p>}
-      {error && <p>{error}</p>}
-      {/* step3 : pass the users data to Users component  */}
+      {isLoading && <p style={{color:'yellow'}}>Loading users...</p>}
+      {error && <p style={{color:'red'}}>{error}</p>}
+      {users && <Users users={users}/>}
     </div>
   );
 };
