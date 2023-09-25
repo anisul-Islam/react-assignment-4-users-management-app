13e958d50f2461a5c480ea8b2a001a4a019a5ddf
tssabbir <tssabbirhosen@gmail.com>
2023-09-26 02:51:34 +0600

    sove assignment-4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Users from './components/Users';
 
 const App = () => {
   // step1 : declare three states here : users, isLoading, error
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // step2 : use useEffect for fetching the data including updating isLoading and error states
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Failed to fetch data');
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <div className="container">
@@ -13,6 +35,7 @@ const App = () => {
       {isLoading && <p>Loading users...</p>}
       {error && <p>{error}</p>}
       {/* step3 : pass the users data to Users component  */}
+      {users && <Users users={users} />}
     </div>
   );
 };
